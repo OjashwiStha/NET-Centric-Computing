09680914f2c3c4115d480429a25f9437ab7321d9
ojashwistha <107242877+ojashwistha@users.noreply.github.com>
2024-11-19 20:42:38 +0545

    Add project files.

---
+++ swap/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 40, b = 12;
+            Console.WriteLine("Before swapping a= " + a + " b= " + b);
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.Write("After swapping a= " + a + " b= " + b);
+        }
+    }
+}
